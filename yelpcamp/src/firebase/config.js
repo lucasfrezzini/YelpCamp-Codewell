85610e98e623fa5ba1dfa71319c4ac2e1a4b00c2
Lucas Frezzini <luquiitas.f@gmail.com>
2023-02-11 06:01:47 -0300

    Add Firebase in the project

---
+++ yelpcamp/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ_kkA11j3KwVM7-VIX469f8ABQhiQgYE",
+  authDomain: "yelpcamp-frezzini.firebaseapp.com",
+  projectId: "yelpcamp-frezzini",
+  storageBucket: "yelpcamp-frezzini.appspot.com",
+  messagingSenderId: "280600669491",
+  appId: "1:280600669491:web:09dc0deb8c561cadfd2246"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
