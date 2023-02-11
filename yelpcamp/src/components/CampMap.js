8574aca37394e6f605d02321a2ddd675ef5930c0
Lucas Frezzini <luquiitas.f@gmail.com>
2023-02-11 02:43:39 -0300

    Add: include Leaflet for dinamic maps in CampDetail info

--- yelpcamp/src/components/CampMap.js
+++ yelpcamp/src/components/CampMap.js
@@ -1,9 +1,36 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+
 import photoMap from "../assets/img/img_map.png";
+import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
 
 export default function CampMap() {
+    const position = [51.505, -0.09];
+  useEffect(() => {
+    const L = require("leaflet");
+
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    });
+  }, []);
+
   return (
-    <section className="rounded border p-12 place-self-start w-full">
-      <img src={photoMap} className="w-full h-auto" />
+    <section className="rounded border p-12 place-self-start w-full h-auto">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </section>
   );
 }
